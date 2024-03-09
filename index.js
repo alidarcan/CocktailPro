e74076befe492101ce8a422814b1f69360f27c42
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2024-03-09 17:57:15 +0300

    /search 50X bug fixed

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ app.get("/", async (req, res) => {
 
 app.post("/search", async (req, res) => {
   try {
-    req.body[Object.keys(req.body)] = decodeURIComponent(req.body[Object.keys(req.body)])
+    req.body[Object.keys(req.body)] = decodeURIComponent(
+      req.body[Object.keys(req.body)]
+    );
     let result = await axios.get(
       "https://www.thecocktaildb.com/api/json/v1/1/filter.php",
       {
@@ -65,30 +67,30 @@ app.post("/search", async (req, res) => {
   }
 });
 app.post("/searchbar", async (req, res) => {
-    if(req.body.s !== ""){
-        try {
-          let result = await axios.get(
-            "https://www.thecocktaildb.com/api/json/v1/1/search.php",
-            {
-              params: req.body,
-            }
-          );
-          if(result.data["drinks"]){
-              let title = req.body[Object.keys(req.body)].split("_").join(" ");
-              title = title[0].toUpperCase() + title.slice(1).toLowerCase();
-              res.render("gallery.ejs", {
-                data: result.data["drinks"],
-                title: title,
-              });
-          } else{
-              res.redirect("/")
-          }
-        } catch (error) {
-          console.log(error);
+  if (req.body.s !== "") {
+    try {
+      let result = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php",
+        {
+          params: req.body,
         }
-    } else{
-        res.redirect("/")
+      );
+      if (result.data["drinks"]) {
+        let title = req.body[Object.keys(req.body)].split("_").join(" ");
+        title = title[0].toUpperCase() + title.slice(1).toLowerCase();
+        res.render("gallery.ejs", {
+          data: result.data["drinks"],
+          title: title,
+        });
+      } else {
+        res.redirect("/");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/:id", async (req, res) => {
@@ -102,34 +104,38 @@ app.get("/:id", async (req, res) => {
         },
       }
     );
-    let drink = result.data.drinks[0];
-    let ingredients = [];
-    for (let i = 1; i < 10; i++) {
-      if (drink["strIngredient" + i] === null) {
-        break;
+    if (result.data.drinks) {
+      let drink = result.data.drinks[0];
+      let ingredients = [];
+      for (let i = 1; i < 10; i++) {
+        if (drink["strIngredient" + i] === null) {
+          break;
+        }
+        if (drink["strMeasure" + i] === null) {
+          drink["strMeasure" + i] = "";
+        }
+        const element =
+          drink["strMeasure" + i] + " " + drink["strIngredient" + i];
+        ingredients.push(element);
       }
-      if (drink["strMeasure" + i] === null) {
-        drink["strMeasure" + i] = "";
-      }
-      const element =
-        drink["strMeasure" + i] + " " + drink["strIngredient" + i];
-      ingredients.push(element);
-    }
 
-    res.render("index.ejs", {
-      name: drink.strDrink,
-      id: drink.idDrink,
-      glass: drink.strGlass,
-      category: drink.strCategory,
-      instructions: drink.strInstructions,
-      thumb: drink.strDrinkThumb,
-      ingredients: ingredients,
-      alcoholic: drink.strAlcoholic,
-      category_a: categories.a,
-      category_c: categories.c,
-      category_g: categories.g,
-      category_i: categories.i,
-    });
+      res.render("index.ejs", {
+        name: drink.strDrink,
+        id: drink.idDrink,
+        glass: drink.strGlass,
+        category: drink.strCategory,
+        instructions: drink.strInstructions,
+        thumb: drink.strDrinkThumb,
+        ingredients: ingredients,
+        alcoholic: drink.strAlcoholic,
+        category_a: categories.a,
+        category_c: categories.c,
+        category_g: categories.g,
+        category_i: categories.i,
+      });
+    } else{
+      res.redirect("/")
+    }
   } catch (error) {
     console.log(error.response.message);
     res.statusCode(404);
